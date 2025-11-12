6bbe3c6dc83bf94d546fc03b2acbdf550fac96ee
Fatma <fatmahussein355@gmail.com>
2025-11-12 10:09:55 +0300

    fetching data from api and printing filtered results

---
+++ api_data.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "http://universities.hipolabs.com/search?country=Kenya"
+response = requests.get(url)
+data = response.json()
+
+# Filter universities with 'Technology' in their name
+tech_universities = [uni for uni in data if "Technology" in uni["name"]]
+
+for uni in tech_universities:
+    print(uni["name"], "-", uni["web_pages"][0])
